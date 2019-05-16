fe325e0ed77aeeff6577102c8202d744358f54ca
c̡̼̰͕̺̐̆̉̂̑͝o̘̟̙̮̩̮̲̺͍͒̽́̑̈̆̋͒͝m̛̹̘̳̣̝͕̥̿͜͠͞͠m̷̢̮̠͎͎̮͐̏̔̋̅̽̚͞ͅų̷̛̛͙͖͉̓̌̚͢͢͠͡n̨̖̲̭̥̰͎͈̎͛̇̐͑̃i̵̺̥̭͍̱̓̉̆͑̆͛͢s̨̡̭̪̬͈̼̠̤͎̑̄͑͛̃͛͋́͡m̧̧̮̬̬̐̑̾̃͆͌̓̍̂͝m̛̝̦̦̣͇͚̲̈́͂͒̉̕͘̚͘͡i̝̻̣̐͌͐͆̈̚͜ͅl̸̪̹̘͇̜͓͖̄̊̐̊̋̃̅̔̕k̨̛̖͉̟̥̤̠͂̌̾́̎̾̇̊̚͜ <city.obey@gmail.com>
2019-05-15 18:23:50 -0700

    encapsulation (ip)

--- src/_01_introduction_to_encapsulation/EncapsulateTheData.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheData.java
@@ -23,10 +23,10 @@ public class EncapsulateTheData {
 	//2. Create a new JUnit Test case and write tests to verify that 
 	//   the member variables' getters and setters are working
 	
-	int itemsReceived; //must not be negative. All negative arguments get set to 0.
-	float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
-	String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
-	Object memberObj; //can be any object type except String. Strings get turned into objects.
+	private int itemsReceived; //must not be negative. All negative arguments get set to 0.
+	private float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
+	private String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
+	private Object memberObj; //can be any object type except String. Strings get turned into objects.
 	
 	public static void main(String[] args) {
 		
