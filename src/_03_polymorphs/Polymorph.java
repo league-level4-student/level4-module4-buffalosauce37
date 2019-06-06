6e005462bc253db000fe28193fbd687e6ad7aa75
ilabstaff <ilabstaff@172.16.116.69>
2019-06-05 18:25:21 -0700

    polymorph (ip)

--- src/_03_polymorphs/Polymorph.java
+++ src/_03_polymorphs/Polymorph.java
@@ -3,17 +3,33 @@ package _03_polymorphs;
 import java.awt.Graphics;
 
 public abstract class Polymorph {
-    int x;
-    int y;
-    
-    Polymorph(int x, int y){
-   	 this.x = x;
-   	 this.y = y;
-    }
-    
-    public void update(){
-   	 
-    }
-    
-    public abstract void draw(Graphics g);
+	private int x;
+	private int y;
+
+	int getX() {
+		return this.x;
+	}
+
+	void setX() {
+		this.x = x;
+	}
+
+	int getY() {
+		return this.y;
+	}
+
+	void setY() {
+		this.y = y;
+	}
+
+	Polymorph(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void update() {
+
+	}
+
+	public abstract void draw(Graphics g);
 }
--- src/_03_polymorphs/PolymorphWindow.java
+++ src/_03_polymorphs/PolymorphWindow.java
@@ -10,46 +10,54 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class PolymorphWindow extends JPanel implements ActionListener{
-    public static final int WIDTH = 900;
-    public static final int HEIGHT = 600;
-    
-    private JFrame window;
-    private Timer timer;
-    
-    Polymorph bluePoly;
-    
-    public static void main(String[] args) {
-   	 new PolymorphWindow().buildWindow();
-    }
-    
-    public void buildWindow(){
-   	 window = new JFrame("IT'S MORPHIN' TIME!");
-   	 window.add(this);
-   	 window.getContentPane().setPreferredSize(new Dimension(500, 500));
-   	 window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-   	 window.pack();
-   	 window.setVisible(true);
-   	 
-   	 bluePoly = new BluePolymorph(50, 50);
-   	 
-   	 timer = new Timer(1000 / 30, this);
-   	 timer.start();
-    }
-    
-    public void paintComponent(Graphics g){
-    //draw background
-   	 g.setColor(Color.LIGHT_GRAY);
-   	 g.fillRect(0, 0, 500, 500);
-   	
-   	 //draw polymorph
-   	 bluePoly.draw(g);
-    }
+public class PolymorphWindow extends JPanel implements ActionListener {
+	private int width;
+	private int height;
+	
+	int getWidth() {
+		return this.width;
+	}
+	void setWidth() {
+		this.width = width ;
+		
+	}
+	
+	private JFrame window;
+	private Timer timer;
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-   	 repaint();
-   	 bluePoly.update();
-   	 
-    }
+	Polymorph bluePoly;
+
+	public static void main(String[] args) {
+		new PolymorphWindow().buildWindow();
+	}
+
+	public void buildWindow() {
+		window = new JFrame("IT'S MORPHIN' TIME!");
+		window.add(this);
+		window.getContentPane().setPreferredSize(new Dimension(500, 500));
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.pack();
+		window.setVisible(true);
+
+		bluePoly = new BluePolymorph(50, 50);
+
+		timer = new Timer(1000 / 30, this);
+		timer.start();
+	}
+
+	public void paintComponent(Graphics g) {
+		// draw background
+		g.setColor(Color.LIGHT_GRAY);
+		g.fillRect(0, 0, 500, 500);
+
+		// draw polymorph
+		bluePoly.draw(g);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		repaint();
+		bluePoly.update();
+
+	}
 }
